5ddb6057454167f136bffd073d02f6e60fd5a0b5
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2024-09-04 09:18:56 +0800

    Update recover.py

--- recover.py
+++ recover.py
@@ -50,8 +50,8 @@ summary_message = "serv00-singbox-nezha 恢复操作结果：\n"
 
 # 默认恢复命令
 default_restore_command = [
-    "ps aux | grep -v grep | grep run > /dev/null || $HOME/sb/servesb.sh >/dev/null 2>&1 &",
-    "ps aux | grep -v grep | grep nezha-agent > /dev/null || nohup $HOME/nezha-agent/nezha-agent.sh >/dev/null 2>&1 &"
+    "ps aux | grep -v grep | grep run > /dev/null || $HOME/sb/servesb.sh >/dev/null 2>&1 ",
+    "ps aux | grep -v grep | grep nezha-agent > /dev/null || nohup $HOME/nezha-agent/nezha-agent.sh >/dev/null 2>&1 "
 ]
 
 # 遍历服务器列表并执行恢复操作
